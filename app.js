3d30cdbc3c22d5b557841ee6df08c96bd7db6057
Lucas dos Anjos <lucas.anjospro@gmail.com>
2024-09-24 16:48:28 -0300

    add: animaçoes de target estrelas + guia estrelar

--- app.js
+++ app.js
@@ -1,56 +1,95 @@
-// document.querySelector('a-scene').setAttribute('hello-world', '');
-
-AFRAME.registerComponent('hello-world', {
-    init: function () {
-      console.log('Hello, World!');
-    }
-});
-
 AFRAME.registerComponent('set-stars', {
+    schema: {
+        // Onde vai os atributos, se necessario
+    },
+
     init: function() {
+        this.miraMoto = document.getElementById('mira-moto');
+        this.setaMoto = document.getElementById('seta-moto');
+        this.camera = document.getElementById('camera-target');
         var coordenadas = [
-        {
-            constelacao: { x: 0, y: 12, z: -13 },
-            estrela1: { x: 18.05943, y: 1.6, z: -4.37369 },
-            estrela2: { x: 21.0724, y: 8.4112, z: 3.70871 },
-            estrela3: { x: 10.48846, y: 16.60277, z: -0.8941 },
-            estrela4: { x: 2.07132, y: 13.71825, z: -9.23096 },
-            estrela5: { x: -8.18114, y: 19.48466, z: -3.88721 }
-        }
+            {
+                constelacao: { x: 0, y: 12, z: -13 },
+                estrela1: { x: 18.05943, y: 1.6, z: -4.37369 },
+                estrela2: { x: 21.0724, y: 8.4112, z: 3.70871 },
+                estrela3: { x: 10.48846, y: 16.60277, z: -0.8941 },
+                estrela4: { x: 2.07132, y: 13.71825, z: -9.23096 },
+                estrela5: { x: -8.18114, y: 19.48466, z: -3.88721 }
+            }
         ];
+        // Escolher uma coordenada ativa aleatória
         var coordenadaAtiva = coordenadas[Math.floor(Math.random() * coordenadas.length)];
         console.log(coordenadaAtiva);
 
-        // Seta as coordenadas Constelação e estrelas
+        // Seta as posições da Constelação e estrelas
         this.el.object3D.position.set(coordenadaAtiva.constelacao.x, coordenadaAtiva.constelacao.y, coordenadaAtiva.constelacao.z);
-        this.estrelas = this.el.querySelectorAll('.clickable');
         
-        // Usando arrow function para preservar o contexto de `this`
+        // Loop pela array this.estrelas
+        this.estrelas = this.el.querySelectorAll('.clickable');
         for (let i = 0; i < this.estrelas.length; i++) {
             this.estrelas[i].object3D.position.set(coordenadaAtiva['estrela' + (i + 1)].x, coordenadaAtiva['estrela' + (i + 1)].y, coordenadaAtiva['estrela' + (i + 1)].z);
 
             this.estrelas[i].setAttribute('look-at', '#camera-target');
-            this.estrelas[i].addEventListener('mouseenter', (e) => this.colidiuStar(e));
+            this.estrelas[i].addEventListener('mouseenter', (e) => this.collisionStar(e));
         }
 
+        // Seta a estrela alvo do momento
         this.targetStar = 0;
         this.activeStar(this.targetStar);
+    },
+
+    tick: function (time, timeDelta) {
+        let local = this.estrelas[this.targetStar].object3D.position.clone();
+        this.camera.object3D.worldToLocal(local);
+
+        let angleDeg = Math.atan2(-local.y - 0, local.x - 0) * 180 / Math.PI + 180;
+        angleDeg -= 135;
+        
+        this.setaMoto.style.transform = `translate(-50%, -50%) rotate(${angleDeg}deg)`;
     },
 
     activeStar: function (numStar) {
         this.targetStar = numStar;
         this.estrelas[numStar].setAttribute('opacity', 1);
+        this.estrelas[numStar].setAttribute('animation', 'property: scale; to: 2 1.7 1; dir: alternate; dur: 1000; easing: easeInOutQuad; loop: true');
     },
 
-    colidiuStar: function (e) {
+    collisionStar: function (e) {
         console.log(e.target.id);
-        console.log(this.estrelas[this.targetStar].id);  // Agora `this.estrelas` não será mais `undefined`
+        console.log(this.estrelas[this.targetStar].id);
+
         if(e.target.id == this.estrelas[this.targetStar].id && this.targetStar < 4) {
+            this.miraMoto.classList.add("animate-brilho");
+            this.estrelas[this.targetStar].removeAttribute('animation');
             this.targetStar++;
             this.activeStar(this.targetStar);
+            setTimeout(()=> this.miraMoto.classList.remove("animate-brilho"), 800);
+        }
+        if(e.target.id == 'star5' && this.estrelas[this.targetStar].id == 'star5') {
+            alert('fim de jogo');
         }
     }
 });
+
+
+// AFRAME.registerComponent('look-at-target', {
+//     schema: {
+//       target: { type: 'selector' }
+//     },
+  
+//     tick: function () {
+//       // Obtém a posição da câmera e do alvo
+//       var cameraPos = this.el.object3D.getWorldPosition();
+//       var targetPos = this.data.target.object3D.getWorldPosition();
+  
+//       // Calcula a direção da seta
+//       var direction = new THREE.Vector3();
+//       direction.subVectors(targetPos, cameraPos).normalize();
+  
+//       // Define a rotação da seta para apontar na direção calculada
+//       this.el.object3D.lookAt(targetPos);
+//     }
+//   });
 
 
 // AFRAME.registerComponent('set-stars', {
@@ -110,22 +149,3 @@ AFRAME.registerComponent('set-stars', {
 //         // }
 //     }
 // });
-
-AFRAME.registerComponent('scale-on-mouseenter', {
-// schema: {
-//   to: {default: '2.5 2.5 2.5', type: 'vec3'}
-// },
-
-    init: function () {
-        var data = this.data;
-
-        var el = this.el;
-        this.el.addEventListener('mouseenter', function (e) {
-        console.log(e.target)
-        el.object3D.scale.set(1.3, 1.3, 1.3);
-        });
-        this.el.addEventListener('mouseleave', function () {
-        el.object3D.scale.set(1, 1, 1);
-        });
-    }
-});
